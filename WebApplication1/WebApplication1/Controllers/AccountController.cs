ac3b6469f9b97d8a5d0d79acca9c503fef9cd4fa
Leona <leonaorz@gmail.com>
2017-01-08 21:53:48 +0800

    lll

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -21,7 +21,7 @@ namespace WebApplication1.Controllers
         public AccountController()
         {
             var name = 999;
-            var show = "kkk";
+            var show = "lll";
             var age = 120;
             var address = "233333";
         }
