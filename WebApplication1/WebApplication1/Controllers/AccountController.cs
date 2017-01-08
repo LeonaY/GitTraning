fbd0e30afb62cbf470375b426b9d95f8ffe5a70c
Leona <leonaorz@gmail.com>
2017-01-08 21:12:21 +0800

    tst

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace WebApplication1.Controllers
 
         public AccountController()
         {
+
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
