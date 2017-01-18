b93f9231dd32f35aad2c9dd5946c05ff94c9f5ef
Leona <leonaorz@gmail.com>
2017-01-18 21:53:25 +0800

    test

---
+++ AzureADTraning/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AzureADTraning
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
